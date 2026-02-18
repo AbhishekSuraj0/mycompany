e55e90196d612ed71ea05e41754dcee2fab2e1d1
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-02-18 11:36:53 +0530

    fgg

---
+++ index.js
@@ -0,0 +1,53 @@
+
+const local = 'http://localhost:5000/'
+
+async function loginUser() {
+    const usernumber = document.getElementById("usernumber").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+
+    let deviceId = crypto.randomUUID();
+    localStorage.setItem("deviceId", deviceId);
+    const device = { deviceId }
+
+
+
+    if (usernumber === "" || password === "") {
+        message.style.color = "red";
+        message.innerHTML = "Please fill all fields ❌";
+        return;
+    }
+    loadUsers(usernumber, password, device);
+
+};
+
+async function loadUsers(a, b ,c) {
+    try {
+        const res = await fetch(`${local}u`);
+        const data = await res.json();
+
+        const user = data.find(u => a == u.number);
+
+        if (user) {
+            if (a == user.number && b == user.password) {
+
+
+                localStorage.setItem("isLoggedIn", "true");
+                localStorage.setItem("usernumber", user.number);
+                localStorage.setItem("position", user.position);
+                window.location.href = "../main/"
+
+            } else {
+                alert("Please Inter Correct Password")
+            }
+        } else {
+            alert("❌ User Not Found");
+        }
+
+
+
+    } catch (err) {
+        console.log("Fetch Error:", err);
+    }
+}
